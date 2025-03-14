0e69dbc464babf63fac5515f815f4fe3010d909a
Claudio Olivares <claudio.olivares@imagemaker.com>
2025-03-14 17:32:41 -0300

    Corrige configuración para Netlify

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
+  base: './', // Asegura rutas relativas
+  build: {
+    outDir: 'dist', // Asegura que la carpeta de salida sea 'dist'
+  },
 })
 
